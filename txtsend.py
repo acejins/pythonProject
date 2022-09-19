8f8e30669a47e960a7e26b3cf9a10b46301f0409
akj <abubu24@nate.com>
2022-09-19 22:15:42 +0900

    print

--- txtsend.py
+++ txtsend.py
@@ -1,14 +1,25 @@
-import sys
+# import sys
+#
+#
+# def printsave(*a):
+#     file = open('x.txt', 'a')
+#
+#     print(*a)
+#     print(*a, file=file)
+#
+#     file.close()
+#
+#
+# if __name__ == '__main__':
+#     printsave('PyCharm')
 
 
-def printsave(*a):
-    file = open('x.txt', 'a')
-
-    print(*a)
-    print(*a, file=file)
-
-    file.close()
-
-
-if __name__ == '__main__':
-    printsave('PyCharm')
+a = 98765.4321
+print(f"{a}")       # 98765.4321
+print(f"{a:>20}")   #           98765.4321
+print(f"{a:<20}")   # 98765.4321          /
+print(f"{a:^20}")   #     98765.4321      /
+print(f"{a:*^20}")  # *****98765.4321*****
+print(f"{438765434567654345:,}")  # 438,765,434,567,654,345
+print(f"{438765434567654345:,>50,}")
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,438,765,434,567,654,345
